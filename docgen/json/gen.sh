24f598426ec6ee134cc87776a111af55a278f48c
Steve Springett <steve@springett.us>
2021-12-29 14:07:49 -0600

    Updated json-schema-for-humans to v0.39.3
    Signed-off-by: Steve Springett <steve@springett.us>

--- docgen/json/gen.sh
+++ docgen/json/gen.sh
@@ -4,7 +4,7 @@ mkdir -p docs/{1.2,1.3,1.4-SNAPSHOT}
 
 # Check to see if generate-schema-doc is executable and is in the path. If not, install JSON Schema for Humans.
 if ! [ -x "$(command -v generate-schema-doc)" ]; then
-  pip3 install json-schema-for-humans==0.36.1
+  pip3 install json-schema-for-humans==0.39.3
 fi
 
 generate () {
@@ -18,7 +18,7 @@ generate () {
       SCHEMA_FILE='../../schema/bom-'$version'.schema.json'
   fi
   echo $SCHEMA_FILE
-  generate-schema-doc --config no_link_to_reused_ref --config no_show_breadcrumbs --config title="$title" --config template_name=$(pwd)'/templates/cyclonedx' --minify $SCHEMA_FILE 'docs/'$version'/index.html'
+  generate-schema-doc --config no_link_to_reused_ref --config no_show_breadcrumbs --config title="$title" --config custom_template_path=$(pwd)'/templates/cyclonedx/base.html' --minify $SCHEMA_FILE 'docs/'$version'/index.html'
   sed -i -e "s/\${quotedTitle}/\"$title\"/g" 'docs/'$version'/index.html'
   sed -i -e "s/\${title}/$title/g" 'docs/'$version'/index.html'
   sed -i -e "s/\${version}/$version/g" 'docs/'$version'/index.html'
