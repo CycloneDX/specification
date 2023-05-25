6a11336fdd7cd7643950da3ac38fb3125c22f39a
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-05-25 16:16:37 +0200

    tidy
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/src/test/js/json-schema-lint-tests.js
+++ tools/src/test/js/json-schema-lint-tests.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import assert from 'assert'
 import {readFile} from 'fs/promises'
 import {dirname, basename, join} from 'path'
 import {fileURLToPath} from 'url'
@@ -9,15 +10,19 @@ import addFormats from 'ajv-formats'
 import addFormats2019 from 'ajv-formats-draft2019'
 import {glob} from 'glob'
 
+// region config
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const schemaDir = join(__dirname, '..', '..', '..', '..', 'schema')
+const bomSchemasGlob = 'bom-*.schema.json'
+const schemaDir = join(dirname(fileURLToPath(import.meta.url)), '..', '..', '..', '..', 'schema')
 
-const [spdxSchema, jsfSchema] = await Promise.all([
+// endregion config
+
+const [spdxSchema, jsfSchema, bomSchemas] = await Promise.all([
     readFile(join(schemaDir, 'spdx.schema.json'), 'utf-8').then(JSON.parse),
-    readFile(join(schemaDir, 'jsf-0.82.schema.json'), 'utf-8').then(JSON.parse)
+    readFile(join(schemaDir, 'jsf-0.82.schema.json'), 'utf-8').then(JSON.parse),
+    glob(join(schemaDir, bomSchemasGlob)).then(l => l.sort())
 ])
-const bomSchemas = (await glob(join(schemaDir, 'bom-*.schema.json'))).sort()
+assert.notStrictEqual(bomSchemas.length, 0)
 
 /**
  * @return {Ajv}
@@ -28,8 +33,10 @@ function getAjv() {
         useDefaults: false,
         // main idea is to be as strict as possible
         strict: true,
-        // this parser has issues with the oneOf-required in `{oneOf:[{reqiored:['a']},{required:['b']}], properties:{a:{},b:{}}}`
-        strictRequired: false,
+        // this parser has issues with the oneOf-required in
+        // `{ type: 'object', oneOf:[{required:['a']},{required:['b']}], properties:{a:{...},b:{...}} }`
+        // so lets simply log them, do not throw errors on them.
+        strictRequired: 'log',
         strictSchema: true,
         addUsedSchema: false,
         schemas: {
@@ -39,7 +46,8 @@ function getAjv() {
     });
     addFormats(ajv)
     addFormats2019(ajv, {formats: ['idn-email']})
-    // there is just no working implementation for format "iri-reference": see https://github.com/luzlab/ajv-formats-draft2019/issues/22
+    // there is just no working implementation for format "iri-reference"
+    // see https://github.com/luzlab/ajv-formats-draft2019/issues/22
     ajv.addFormat('iri-reference', true)
     return ajv
 }
