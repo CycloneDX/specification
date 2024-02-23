9ad0ebe62480181959066eea90436179afd48add
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-02-23 17:23:39 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/src/test/proto-test.sh
+++ tools/src/test/proto-test.sh
@@ -1,37 +1,62 @@
 #!/usr/bin/env bash
-set -ue
+set -uex
 
 THIS_DIR="$(dirname "$0")"
 REPO_ROOT="$(realpath "${THIS_DIR}/../../..")"
 
-if [[ -n "${CI:-}" ]]
-then
-  LOG_FORMAT="github-actions"
-else
-  LOG_FORMAT="json"
-fi
+# paths relative to $REPO_ROOT
+BUF_CONFIG='buf.yaml'
+SCHEMA_DIR='schema'
 
 function test-schema-lint () {
- echo '> lint schema files' >&2
+  echo '> lint schema files' >&2
 
-  docker run \
-    --volume "${REPO_ROOT}:/workspace" \
+  if [[ -n "${CI:-}" ]]
+  then
+    LOG_FORMAT='github-actions'
+  else
+    LOG_FORMAT='text'
+  fi
+
+  docker run --rm \
+    --volume "${REPO_ROOT}/${SCHEMA_DIR}:/workspace/${SCHEMA_DIR}:ro" \
+    --volume "${REPO_ROOT}/${BUF_CONFIG}:/workspace/buf.yaml:ro" \
     --workdir /workspace \
     bufbuild/buf:1.29.0 \
-    lint --error-format "$LOG_FORMAT"
+      lint \
+      --error-format "$LOG_FORMAT" \
+      --config 'buf.yaml'
 }
 
 
 function test-schema-breaking () {
   echo '> test schema for breaking changes' >&2
 
-  return 0  # WIP
+  if [[ -n "${CI:-}" ]]
+  then
+    LOG_FORMAT='github-actions'
+  else
+    LOG_FORMAT='text'
+  fi
 
-  docker run \
-    --volume "${REPO_ROOT}:/workspace" \
-    --workdir /workspace \
-    bufbuild/buf:1.29.0 \
-    breaking 'schema/bom-1.6.proto' --against 'schema/bom-1.5.proto' --error-format "$LOG_FORMAT"
+  function run-test() {
+    echo "> new:${1} -VS- old:${2}" >&2
+    # stick with the original paths, so the reporting makes sense...
+    docker run --rm \
+      --volume "${REPO_ROOT}/${SCHEMA_DIR}/bom-${1}.proto:/workspace/${SCHEMA_DIR}/bom-${1}.proto:ro" \
+      --volume "${REPO_ROOT}/${SCHEMA_DIR}/bom-${2}.proto:/workspace/${SCHEMA_DIR}_old/bom-${1}.proto:ro" \
+      --workdir /workspace \
+      bufbuild/buf:1.29.0 \
+        breaking "${SCHEMA_DIR}/" \
+        --against "${SCHEMA_DIR}_old/" \
+        --error-format "$LOG_FORMAT" \
+        --config '{"version":"v1","breaking":{"use":["FILE","WIRE_JSON"],"except":["FILE_SAME_PACKAGE"]}}'
+        # scope is to detect changes from one version to the other -> so ignore "FILE_SAME_PACKAGE"
+  }
+
+  run-test '1.6' '1.5'
+  run-test '1.5' '1.4'
+  run-test '1.4' '1.3'
 }
 
 function test-schema-functional () {
