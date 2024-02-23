62f214e2cb630e2ce978f7b55105125ca77d6c35
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-02-23 15:54:06 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/src/test/proto-test.sh
+++ tools/src/test/proto-test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -uex
+set -ue
 
 THIS_DIR="$(dirname "$0")"
-REPO_ROOT="$(realpath "$THIS_DIR/../../..")"
+REPO_ROOT="$(realpath "${THIS_DIR}/../../..")"
 
 if [[ -n "${CI:-}" ]]
 then
@@ -11,23 +11,57 @@ else
   LOG_FORMAT="json"
 fi
 
-# lint protobuf schema files
-docker run \
-  --volume "$REPO_ROOT:/workspace" \
-  --workdir /workspace \
-  bufbuild/buf:1.29.0 \
-  lint --error-format "$LOG_FORMAT"
+function test-schema-lint () {
+ echo '> lint schema files' >&2
 
-# check protobuf schema files for breaking changes
-docker run \
-  --volume "$REPO_ROOT:/workspace" \
-  --workdir /workspace \
-  bufbuild/buf:1.29.0 \
-  breaking --help
+  docker run \
+    --volume "${REPO_ROOT}:/workspace" \
+    --workdir /workspace \
+    bufbuild/buf:1.29.0 \
+    lint --error-format "$LOG_FORMAT"
+}
 
-# test all examples against the schema files
-# mkdir -p proto-test
-# for filename in resources/1.3/*.textproto;
-# do
-#   protoc --proto_path=../../../schema/ --encode=cyclonedx.v1_3.Bom bom-1.3-SNAPSHOT.proto < $filename | protoc --proto_path=../../../schema/ --decode=cyclonedx.v1_3.Bom bom-1.3-SNAPSHOT.proto > proto-test/${filename##*/}
-# done
+
+function test-schema-breaking () {
+  echo '> test schema for breaking changes' >&2
+
+  return 0  # WIP
+
+  docker run \
+    --volume "${REPO_ROOT}:/workspace" \
+    --workdir /workspace \
+    bufbuild/buf:1.29.0 \
+    breaking 'schema/bom-1.6.proto' --against 'schema/bom-1.5.proto' --error-format "$LOG_FORMAT"
+}
+
+function test-schema-functional () {
+  echo '> test all examples against the schema files' >&2
+
+  return 0 # WIP
+
+  mkdir -p proto-test
+  for filename in resources/1.3/*.textproto;
+  do
+    protoc --proto_path=../../../schema/ --encode=cyclonedx.v1_3.Bom bom-1.3-SNAPSHOT.proto < $filename | protoc --proto_path=../../../schema/ --decode=cyclonedx.v1_3.Bom bom-1.3-SNAPSHOT.proto > proto-test/${filename##*/}
+  done
+}
+
+case "${1:-test}" in
+  'lint')
+    test-schema-lint
+    ;;
+  'breaking')
+    test-schema-breaking
+    ;;
+  'functional')
+    test-schema-functional
+    ;;
+  'test')
+    test-schema-lint
+    test-schema-breaking
+    test-schema-functional
+    ;;
+  *)
+    echo 'unexpected argument. known arguments: lint,breaking,functional,test'
+    exit 1
+esac
