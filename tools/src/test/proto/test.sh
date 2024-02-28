405fd3e47bf8e98df4951c6b459af4a5210c6725
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-02-28 14:06:20 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/src/test/proto/test.sh
+++ tools/src/test/proto/test.sh
@@ -10,12 +10,9 @@ TEST_RES_DIR='tools/src/test/resources'
 
 REMOTE="https://github.com/${GITHUB_REPOSITORY:-CycloneDX/specification}.git"
 
-DOCKER_IMG_NAME='cdx_schema_testing_protobuf'
 
-function prepare() {
-  echo '> preparing runner image' >&2
-  docker build --tag "$DOCKER_IMG_NAME" "$THIS_PATH"
-}
+## ----
+
 
 function schema-lint () {
   echo '> lint schema files' >&2
@@ -31,8 +28,8 @@ function schema-lint () {
     --volume "${ROOT_PATH}/${SCHEMA_DIR}:/workspace/${SCHEMA_DIR}:ro" \
     --volume "${THIS_PATH}/buf_lint.yaml:/workspace/buf.yaml:ro" \
     --workdir '/workspace' \
-    "$DOCKER_IMG_NAME" \
-      buf lint --path "$SCHEMA_DIR" \
+    bufbuild/buf:1.29.0 \
+      lint --path "$SCHEMA_DIR" \
       --config 'buf.yaml' \
       --error-format "$LOG_FORMAT"
 
@@ -51,15 +48,19 @@ function schema-breaking-version () {
   fi
 
   function compare() {
-    echo ">> compare new:${1} -VS- old:${2}" >&2
+    NEW="${1}"
+    OLD="${2}"
+
+    echo ">> compare new:${NEW} -VS- old:${OLD}" >&2
     # stick with the original paths, so the reporting makes sense...
     docker run --rm \
-      --volume "${ROOT_PATH}/${SCHEMA_DIR}/bom-${1}.proto:/workspace/${SCHEMA_DIR}/bom-${1}.proto:ro" \
-      --volume "${ROOT_PATH}/${SCHEMA_DIR}/bom-${2}.proto:/workspace/${SCHEMA_DIR}_old/bom-${1}.proto:ro" \
+      --volume "${ROOT_PATH}/${SCHEMA_DIR}/bom-${NEW}.proto:/workspace/${SCHEMA_DIR}/bom-${NEW}.proto:ro" \
+      --volume "${ROOT_PATH}/${SCHEMA_DIR}/bom-${OLD}.proto:/workspace/${SCHEMA_DIR}_old/bom-${NEW}.proto:ro" \
       --volume "${THIS_PATH}/buf_breaking-version.yaml:/workspace/buf.yaml:ro" \
       --workdir '/workspace' \
-      "$DOCKER_IMG_NAME" \
-        buf breaking "$SCHEMA_DIR" --against "${SCHEMA_DIR}_old" \
+      bufbuild/buf:1.29.0 \
+        breaking "$SCHEMA_DIR" \
+        --against "${SCHEMA_DIR}_old" \
         --config 'buf.yaml' \
         --error-format "$LOG_FORMAT"
   }
@@ -85,8 +86,9 @@ function schema-breaking-remote () {
     --volume "${ROOT_PATH}/${SCHEMA_DIR}:/workspace/${SCHEMA_DIR}:ro" \
     --volume "${THIS_PATH}/buf_breaking-remote.yaml:/workspace/buf.yaml:ro" \
     --workdir '/workspace' \
-    "$DOCKER_IMG_NAME" \
-      buf breaking "$SCHEMA_DIR" --against "${REMOTE}#subdir=${SCHEMA_DIR}" \
+    bufbuild/buf:1.29.0 \
+      breaking "$SCHEMA_DIR" \
+      --against "${REMOTE}#subdir=${SCHEMA_DIR}" \
       --config 'buf.yaml' \
       --error-format "$LOG_FORMAT"
 
@@ -99,19 +101,20 @@ function schema-functional () {
   function validate() {
     FILE="$1"
     SCHEMA_VERS="$2"
+    SCHEMA_FILE="bom-${SCHEMA_VERS}.proto"
     MESSAGE="cyclonedx.v${SCHEMA_VERS/./_}.Bom"
 
-    echo ">> validate ${FILE} as ${MESSAGE}" >&2
+    echo ">> validate ${FILE} as ${MESSAGE} of ${SCHEMA_FILE}" >&2
 
     docker run --rm \
       --volume "${ROOT_PATH}/${SCHEMA_DIR}:/workspace/${SCHEMA_DIR}:ro" \
       --volume "${FILE}:/workspace/test_res:ro" \
       --workdir '/workspace' \
-      "$DOCKER_IMG_NAME" \
-        buf convert "${SCHEMA_DIR}/bom-${SCHEMA_VERS}.proto" \
-          --type "$MESSAGE" \
-          --from 'test_res#format=txtpb' \
-          --to /dev/null
+      bufbuild/buf:1.29.0 \
+        convert "${SCHEMA_DIR}/${SCHEMA_FILE}" \
+        --type "$MESSAGE" \
+        --from 'test_res#format=txtpb' \
+        --to /dev/null
   }
 
   shopt -s globstar
@@ -121,47 +124,32 @@ function schema-functional () {
     validate "$test_res" "$SCHEMA_VERS"
   done
 
-  echo 'TODO' # @TODO
-  return 0 # WIP
-
-  ## buf convert schema/bom-1.6.proto --type cyclonedx.v1_6.Bom --from tools/src/test/resources/1.6/_test.textproto'#format=txtpb'
-  ##
-
-  mkdir -p proto-test
-  for filename in resources/*/*.textproto;
-  do
-    protoc --proto_path=../../../schema/ --encode=cyclonedx.v1_3.Bom bom-1.3-SNAPSHOT.proto < $filename |\
-    protoc --proto_path=../../../schema/ --decode=cyclonedx.v1_3.Bom bom-1.3-SNAPSHOT.proto > proto-test/${filename##*/}
-  done
-
   echo '>> OK.' >&2
 }
 
+
+## ----
+
+
 case "${1:-all}" in
   'schema-lint')
-    prepare
     schema-lint
     ;;
   'schema-breaking-version')
-    prepare
     schema-breaking-version
     ;;
   'schema-breaking-remote')
-    prepare
     schema-breaking-remote
     ;;
   'schema-breaking')
-    prepare
     schema-breaking-version
     schema-breaking-remote
     ;;
   'schema-functional')
-    prepare
     schema-functional
     ;;
   'all')
     # all the above
-    prepare
     schema-lint
     schema-breaking-version
     schema-breaking-remote
