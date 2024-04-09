4017ce480224206a00d1bd6969d30d5fc996d2ef
Steve Springett <steve@springett.us>
2024-04-08 21:15:05 -0500

    Updated SPDX license list to 3.23
    Signed-off-by: Steve Springett <steve@springett.us>

--- tools/src/main/java/org/cyclonedx/tools/SpdxXsdGenerator.java
+++ tools/src/main/java/org/cyclonedx/tools/SpdxXsdGenerator.java
@@ -30,7 +30,7 @@ import java.util.Set;
 
 public class SpdxXsdGenerator {
 
-    private static final String SPDX_VERSION = "3.21";
+    private static final String SPDX_VERSION = "3.23";
 
     public static void main(String args[]) throws Exception {
 
