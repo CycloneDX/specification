20467ff9fa7017937ad57282b54bab0b5a2a3934
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-04-10 09:14:58 +0200

    chore: revisit JSON docs gen scripts
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- docgen/json/gen.sh
+++ docgen/json/gen.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
-rm -f -R docs
-mkdir -p docs/{1.2,1.3,1.4,1.5,1.6}
+set -eu
+
+THIS_DIR="$(dirname "$0")"
+SCHEMA_DIR="$(realpath "$THIS_DIR/../../schema")"
+DOCS_DIR="$THIS_DIR/docs"
+TEMPLATES_DIR="$THIS_DIR/templates"
+
+rm -f -R "$DOCS_DIR"
+mkdir -p "$DOCS_DIR/"{1.2,1.3,1.4,1.5,1.6}
 
 # Check to see if generate-schema-doc is executable and is in the path. If not, install JSON Schema for Humans.
-if ! [ -x "$(command -v generate-schema-doc)" ]; then
-  pip3 install json-schema-for-humans==0.47
+if ! [ -x "$(command -v generate-schema-doc)" ]
+then
+  pip3 install -r "$THIS_DIR/requirements.txt"
 fi
 
 generate () {
-  version=$1
-  title='CycloneDX v'$version' JSON Reference'
-  echo Generating $title
-  STRICT_SCHEMA_FILE='../../schema/bom-'$version'-strict.schema.json'
+  version="$1"
+  title="CycloneDX v${version} JSON Reference"
+  echo "Generating: $title"
+
+  SCHEMA_FILE="$SCHEMA_DIR/bom-${version}.schema.json"
+  STRICT_SCHEMA_FILE="$SCHEMA_DIR/bom-${version}-strict.schema.json"
   if [ -f "$STRICT_SCHEMA_FILE" ]; then
-      SCHEMA_FILE='../../schema/bom-'$version'-strict.schema.json'
-  else
-      SCHEMA_FILE='../../schema/bom-'$version'.schema.json'
+      SCHEMA_FILE="$STRICT_SCHEMA_FILE"
   fi
-  echo $SCHEMA_FILE
-  generate-schema-doc --config no_link_to_reused_ref --config no_show_breadcrumbs --config no_collapse_long_descriptions --deprecated-from-description --config title="$title" --config custom_template_path=$(pwd)'/templates/cyclonedx/base.html' --minify $SCHEMA_FILE 'docs/'$version'/index.html'
-  sed -i -e "s/\${quotedTitle}/\"$title\"/g" 'docs/'$version'/index.html'
-  sed -i -e "s/\${title}/$title/g" 'docs/'$version'/index.html'
-  sed -i -e "s/\${version}/$version/g" 'docs/'$version'/index.html'
+
+  echo "$SCHEMA_FILE"
+  generate-schema-doc \
+    --config no_link_to_reused_ref \
+    --config no_show_breadcrumbs \
+    --config no_collapse_long_descriptions \
+    --deprecated-from-description \
+    --config title="$title" \
+    --config custom_template_path="$TEMPLATES_DIR/cyclonedx/base.html" \
+    --minify "$SCHEMA_FILE" \
+     "$DOCS_DIR/$version/index.html"
+
+  sed -i -e "s/\${quotedTitle}/\"$title\"/g" "$DOCS_DIR/$version/index.html"
+  sed -i -e "s/\${title}/$title/g" "$DOCS_DIR/$version/index.html"
+  sed -i -e "s/\${version}/$version/g" "$DOCS_DIR/$version/index.html"
 }
 
 generate 1.2
