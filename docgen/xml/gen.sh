1572344a73dc04120af269daed3e7a8f07d1765c
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-01-22 10:52:03 +0100

    docs: docsgen latest first
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- docgen/xml/gen.sh
+++ docgen/xml/gen.sh
@@ -38,11 +38,11 @@ generate () {
     title="$title"
 }
 
-generate 1.0
-generate 1.1
-generate 1.2
-generate 1.3
-generate 1.4
-generate 1.5
-generate 1.6
 generate 1.7
+generate 1.6
+generate 1.5
+generate 1.4
+generate 1.3
+generate 1.2
+generate 1.1
+generate 1.0
